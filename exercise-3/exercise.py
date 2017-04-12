ae3df385d1042e692f637f6c0eabda872541e7f4
AW5 <fightwzh@outlook.com>
2017-04-11 20:31:44 -0700

    Finish exercise 1-3

--- exercise-3/exercise.py
+++ exercise-3/exercise.py
@@ -4,6 +4,28 @@
 # and the `guess`), and prints out an appropriate message based on how close
 # the guess is to the target:
 #
+
+def print_hot_or_cold(target,guess):
+    a = abs(guess - target)
+    if a<1:
+        return("got it!")
+    elif a<=1:
+        return("scalding hot")
+    elif a<=3:
+        return("very warm")
+    elif a<=5:
+        return("warm")
+    elif a<=8:
+        return("cold")
+    elif a<=13:
+        return("very cold")
+    else:
+        return("icy freezing miserably cold")
+
+print_hot_or_cold(3,23)
+
+
+
 # Distance    Message
 # -------------------
 # The same    "got it!"
@@ -23,11 +45,32 @@
 # and prompts the user for a guess using the `input()` method. Your function should
 # then print how close the user's guess is (use your previous function!). Note that
 # you will need to convert the input into a number.
+
+
+def guess_number(target):
+    a = input("Input the guess")
+    if target==int(a):
+        return("You got it")
+    else:
+        return("Try one more time")
+
+guess_number(5)
+
 #
 # Once you have a single guess working, modify your function so that the user can
 # make MULTIPLE guesses. You can either do this using a loop (see the next module)
 # or by simply calling your `guess_number() method again IF the user didn't get
 # the answer right. This is an example of **recursion**.
+
+def guess_number(target):
+    '''input target, and let other people guess until he/she get the target'''
+    a = input("Input the guess")
+    while target!=int(a):
+        a=input("Another try")
+    else:
+        return("You got it")
+
+guess_number(3)
 
 
 
